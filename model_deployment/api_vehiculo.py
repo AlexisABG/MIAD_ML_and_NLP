d8f830814d98283cee87b8032f1c67f6cbdab289
AlexisABG <68515474+AlexisABG@users.noreply.github.com>
2022-05-01 12:15:21 -0500

    Add files via upload

---
+++ model_deployment/api_vehiculo.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+import joblib
+from deploy_model_vehicle import predict_proba
+
+app = Flask(__name__)
+
+api = Api(
+    app, 
+    version='1.0', 
+    title='Car Price Predictio API',
+    description='car price prediction API')
+
+ns = api.namespace('predict', 
+     description='vehicle price predictor')
+   
+parser = api.parser()
+
+parser.add_argument(
+    'Year', 
+    type=int, 
+    required=True, 
+    help='vehicle price', 
+    location='args', 
+    )
+
+parser.add_argument(
+    'Mileage', 
+    type=int, 
+    required=True, 
+    help='vehicle Mileage', 
+    location='args', 
+    )
+
+parser.add_argument(
+    'State', 
+    type=str, 
+    required=True, 
+    help='vehicle state', 
+    location='args', 
+    )
+parser.add_argument(
+    'Make', 
+    type=str, 
+    required=True, 
+    help='vehicle brand', 
+    location='args', 
+    )
+
+parser.add_argument(
+    'Model', 
+    type=str, 
+    required=True, 
+    help='vehicle model', 
+    location='args', 
+    )
+
+resource_fields = api.model('Resource', {
+    'result': fields.String,
+})
+
+@ns.route('/')
+class VehicleApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        
+        return {
+         "result": predict_proba(args['Year','Mileage','State','Make','Model'])
+        }, 200
+    
+    
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
